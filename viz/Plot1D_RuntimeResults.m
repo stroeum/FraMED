5aacce60c94e27b69500ac9e6d159aec3bab2c47
Annelisa Esparza <annelisaesparza@MacBook-Pro.lan>
2024-04-02 19:36:17 -0400

    Restructured the output figure to account for runs when the charge is not minimized (i.e. Var::isQMinimized == false).

--- viz/Plot1D_RuntimeResults.m
+++ viz/Plot1D_RuntimeResults.m
@@ -70,14 +70,19 @@ links.array = (1:1:links.tracker)';
 [LinkRuntime.min, LinkRuntime.minIndex] = min(LinkRuntime.values);
 
 % Determining min/max for QMinimization:
-[QMinimization.max, QMinimization.maxIndex] = max(QMinimization.values);
-[QMinimization.min, QMinimization.minIndex] = min(QMinimization.values);
-
+if exist('QMinimization','var')
+    [QMinimization.max, QMinimization.maxIndex] = max(QMinimization.values);
+    [QMinimization.min, QMinimization.minIndex] = min(QMinimization.values);
+end
 
 %% Plot Formatting:
 % Initialize figure layout:
 clf
-tiledlayout(2,2, 'Padding', 'tight', 'TileSpacing', 'compact','Position',[0 0 800 600], 'PositionConstraint','outerposition'); 
+if exist('QMinimization','var')
+    tiledlayout(2,2, 'Padding', 'tight', 'TileSpacing', 'compact','Position',[0 0 800 600], 'PositionConstraint','outerposition'); 
+else
+    tiledlayout(1,3, 'Padding', 'tight', 'TileSpacing', 'loose','Position',[0 0 1200 400], 'PositionConstraint','outerposition');
+end
 
 % Plot number of candidates (row 1, column 1):
 nexttile
@@ -94,36 +99,51 @@ hold off
 nexttile
 hold on
 plot(links.array,Overreach.values);
-set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(Overreach.values))],'TickLabelInterpreter','latex','YAxisLocation','right');
+if exist('QMinimization','var')
+    set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(Overreach.values))],'TickLabelInterpreter','latex','YAxisLocation','right');
+else
+    set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(Overreach.values))],'TickLabelInterpreter','latex');
+end
 xlabel('Step number','Interpreter','latex','FontSize',16);
 ylabel('Maximum candidate overreach (\%)','Interpreter','latex','FontSize',16);
 legend(['Minimum overreach: ',num2str(Overreach.min,'%f'),'\% (step ',num2str(Overreach.minIndex),')',newline,'Maximum overreach: ',num2str(Overreach.max,'%f'),'\% (step ',num2str(Overreach.maxIndex),')'],'Interpreter','latex','location','best', 'box','off','FontSize',10)
 box on
 hold off
 
-% Plot runtime for addition of new link (row 2, column 1):
+% Plot runtime for addition of new link (row 2, column 1 OR row 1, column 3 without QMinimization):
 nexttile
 hold on
 plot(links.array,LinkRuntime.values);
-set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(LinkRuntime.values))],'TickLabelInterpreter','latex','XAxisLocation','top');
+if exist('QMinimization','var')
+    set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(LinkRuntime.values))],'TickLabelInterpreter','latex','XAxisLocation','top');
+else
+    set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(LinkRuntime.values))],'TickLabelInterpreter','latex');
+    xlabel('Step number','Interpreter','latex','FontSize',16);
+end
 ylabel('Runtime for link addition (s)','Interpreter','latex','FontSize',16);
 legend(sprintf(['Minimum link runtime: ',num2str(LinkRuntime.min,'%f'),' seconds (step ',num2str(LinkRuntime.minIndex),')',newline,'Maximum link runtime: ',num2str(LinkRuntime.max,'%f'),' seconds (step ',num2str(LinkRuntime.maxIndex),')']),'Interpreter','latex','location','best', 'box','off','FontSize',10)
 box on
 hold off
 
-% Plot runtime for Q_minimization (row 2, column 2):
-nexttile
-hold on
-plot(links.array,QMinimization.values);
-set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(QMinimization.values))],'TickLabelInterpreter','latex','YAxisLocation','right','XAxisLocation','top');
-ylabel('Runtime for $Q_\mathrm{minimization}$ (s)','Interpreter','latex','FontSize',16);
-legend(['Minimum $Q_\mathrm{minimization}$ runtime: ',num2str(QMinimization.min,'%f'),' seconds (step ',num2str(QMinimization.minIndex),')',newline,'Maximum $Q_\mathrm{minimization}$ runtime: ',num2str(QMinimization.max,'%f'),' seconds (step ',num2str(QMinimization.maxIndex),')'],'Interpreter','latex','location','best', 'box','off','FontSize',10)
-box on
-hold off
+% Plot runtime for Q_minimization (row 2, column 2 OR N/A without QMinimization):
+if exist('QMinimization','var')
+    nexttile
+    hold on
+    plot(links.array,QMinimization.values);
+    set(gca,'FontSize',10,'XLim',[0 links.tracker],'YLim',[0 (max(QMinimization.values))],'TickLabelInterpreter','latex','YAxisLocation','right','XAxisLocation','top');
+    ylabel('Runtime for $Q_\mathrm{minimization}$ (s)','Interpreter','latex','FontSize',16);
+    legend(['Minimum $Q_\mathrm{minimization}$ runtime: ',num2str(QMinimization.min,'%f'),' seconds (step ',num2str(QMinimization.minIndex),')',newline,'Maximum $Q_\mathrm{minimization}$ runtime: ',num2str(QMinimization.max,'%f'),' seconds (step ',num2str(QMinimization.maxIndex),')'],'Interpreter','latex','location','best', 'box','off','FontSize',10)
+    box on
+    hold off
+end
 
 % Format title:
 sgtitle(['(',sims.objectName,'): ',sims.objectType,' discharge runtime results over ',num2str(links.tracker),' steps'],'Interpreter','latex','FontSize',24);
-set(gcf,'Position',[0,0,800,600]);
+if exist('QMinimization','var')
+    set(gcf,'Position',[0,0,800,600]);
+else
+    set(gcf,'Position',[0,0,1200,400]);
+end
 set(gcf,'Resize','off');
 
 % Export figure:
