f22fe0b1151f8c760d0c2a992ca2d9c764486e3d
Annelisa <66029558+annelisae@users.noreply.github.com>
2025-02-05 19:50:47 -0500

    Introduced the basis for a 2D colormap to help visualize magnitude and direction for field values.

--- viz/createRedBlueColorMap.m
+++ viz/createRedBlueColorMap.m
@@ -1,40 +1,44 @@
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-% File Name: createRedBlueColorMap.m                                      %
-% Purpose: Creates a custom color map for negative charge densities (blue)%
-%          and positive charge densities (red). Assumed preference for    %
-%          colorblind-friendly mode, 'white'.                             %
-% Author: Annelisa Esparza                                                %
-% Contact: aesparza2014@my.fit.edu                                        %
+%  File Name: createRedBlueColorMap.m                                     %
+%    Purpose: Creates a custom colormap for scalar or field values, where %
+%             negative values are represented by blue and positive values %
+%             are represented by red.                                     %
+%     Author: Annelisa Esparza                                            %
+%    Contact: annelisa.esparza@my.erau.edu                                %
 % Added Date: February 22, 2022                                           %
-% Last Update: April 4, 2022 - Updated to account for an alpha value.     %
+%    Updates: April 2022 - Updated to account for desired transparency.   %
+%             February 2025 - Introduced colormap for field values.       %
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
-function customColorMap = createRedBlueColorMap(neutralcolor,alphavalue)
+function customColorMap = createRedBlueColorMap(valuetype,alphavalue)
     customColorMap = zeros([101,3]);
-    %customColorMap = zeros([51,3]);
-    customColorMap((((length(customColorMap(:,1))-1)/2)+1),:) = [0.75 0.75 0.75];
-    if strcmp(neutralcolor,'white') == 1 || strcmp(neutralcolor,'White') == 1
+    fullValue = length(customColorMap(:,1));
+    halfValue = ((fullValue-1)/2);
+    if strcmp(valuetype,'scalar') == 1 || strcmp(valuetype,'Scalar') == 1
         % Version 1: blue to white to red
-        increasing_value = linspace((1-alphavalue),1,((length(customColorMap(:,1))-1)/2));
-        decreasing_value = linspace(1,(1-alphavalue),((length(customColorMap(:,1))-1)/2));
-        steady_value = ones([((length(customColorMap(:,1))-1)/2),1]);
-        customColorMap(1:((length(customColorMap(:,1))-1)/2),1) = increasing_value;
-        customColorMap(1:((length(customColorMap(:,1))-1)/2),2) = increasing_value;
-        customColorMap(1:((length(customColorMap(:,1))-1)/2),3) = steady_value;
-        customColorMap((((length(customColorMap(:,1))-1)/2)+2):end,1) = steady_value;
-        customColorMap((((length(customColorMap(:,1))-1)/2)+2):end,2) = decreasing_value;
-        customColorMap((((length(customColorMap(:,1))-1)/2)+2):end,3) = decreasing_value;
-    else
-        % Version 2: blue to grey to red
-        increasing4zero_value = linspace(0,0.75,((length(customColorMap(:,1))-1)/2));
-        decreasing4zero_value = linspace(0.75,0,((length(customColorMap(:,1))-1)/2));
-        increasing4one_value = linspace(0.75,1,((length(customColorMap(:,1))-1)/2));
-        decreasing4one_value = linspace(1,0.75,((length(customColorMap(:,1))-1)/2));
-        customColorMap(1:((length(customColorMap(:,1))-1)/2),1) = increasing4zero_value;
-        customColorMap(1:((length(customColorMap(:,1))-1)/2),2) = increasing4zero_value;
-        customColorMap(1:((length(customColorMap(:,1))-1)/2),3) = decreasing4one_value;
-        customColorMap((((length(customColorMap(:,1))-1)/2)+2):end,1) = increasing4one_value;
-        customColorMap((((length(customColorMap(:,1))-1)/2)+2):end,2) = decreasing4zero_value;
-        customColorMap((((length(customColorMap(:,1))-1)/2)+2):end,3) = decreasing4zero_value;
+        customColorMap((halfValue+1),:) = [0.75 0.75 0.75];
+        increasing_value = linspace((1-alphavalue),1,halfValue);
+        decreasing_value = linspace(1,(1-alphavalue),halfValue);
+        steady_value = ones([halfValue,1]);
+        customColorMap(1:halfValue,1) = increasing_value;
+        customColorMap(1:halfValue,2) = increasing_value;
+        customColorMap(1:halfValue,3) = steady_value;
+        customColorMap((halfValue+2):end,1) = steady_value;
+        customColorMap((halfValue+2):end,2) = decreasing_value;
+        customColorMap((halfValue+2):end,3) = decreasing_value;
+    elseif strcmp(valuetype,'field') == 1 || strcmp(valuetype,'Field') == 1
+        % Version 2: blue to yellow to red
+        quarterValue = floor(halfValue/2)+1;
+        steady_value = ones([halfValue,1]);
+        steady_quarter = ones([quarterValue,1]);
+        half_decrease = linspace(1,0,halfValue+1);
+        quarter_decrease = linspace(1,0,quarterValue);
+        quarter_increase = linspace(0,1,quarterValue);
+        customColorMap(1:quarterValue,3) = quarter_decrease;
+        customColorMap(1:quarterValue,2) = quarter_increase;
+        customColorMap(quarterValue:halfValue+1,2) = steady_quarter;
+        customColorMap(quarterValue:halfValue+1,1) = quarter_increase;
+        customColorMap(halfValue+2:end,1) = steady_value;
+        customColorMap(halfValue+1:end,2) = half_decrease;
     end
 end
