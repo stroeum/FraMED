b5454faff6a551db4bb48bfd9b1fea6741c9b1a7
Annelisa <66029558+annelisae@users.noreply.github.com>
2025-12-08 19:00:11 -0500

    Removed capitalization to ensure consistency across file naming convention (lowercase = function, uppercase = script).

--- viz/ConvertTo3d.m
+++ viz/ConvertTo3d.m
@@ -1,17 +1,17 @@
-function [AA] = ConvertTo3d(A,B)
+function [AA] = convertTo3d(A,sims)
     [M, N] = size(A);
-    AA = zeros(B');
+    AA = zeros([sims.domain.Nx,sims.domain.Ny,sims.domain.Nz]);
     for n=1:N
         jj = n;
         kk = 0;
         for m=1:M
-            if mod(m,B(1)) == 0
-                ii = B(1);
-            elseif mod(m,B(1)) == 1
+            if mod(m,sims.domain.Nx) == 0
+                ii = sims.domain.Nx;
+            elseif mod(m,sims.domain.Nx) == 1
                 ii = 1;
                 kk = kk + 1;
             else
-                ii = mod(m,B(1));
+                ii = mod(m,sims.domain.Nx);
             end
             AA(ii,jj,kk) = A(m,n);
         end
